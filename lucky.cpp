0d6b410d8d794e42fd540047d38114f96451fc27
Diganth M <diganthhema5712@gmail.com>
2026-09-12 16:11:27 +0530

    Implement lucky number finder with new approach
    Added a new approach to find a lucky number that is divisible by both 3 and 5.

--- lucky.cpp
+++ lucky.cpp
@@ -10,3 +10,23 @@ int main() {
     cout << "Lucky number found: " << n << endl;
     return 0;
 }
+
+
+// approach 2
+
+// You are using GCC
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    while(true){
+    int n;
+    cin>> n; 
+    
+    if(n % 3 == 0 && n % 5 == 0){
+        cout<< "Lucky number found: " << n;
+        break;
+    }
+    }
+}
