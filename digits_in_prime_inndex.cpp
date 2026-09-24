3fad0a730c4a74c45c9a8da64bd10a5dc8910e5f
Diganth M <diganthhema5712@gmail.com>
2026-09-24 15:58:32 +0530

    Add functionality to print elements at prime indices
    Implement logic to print elements at prime indices from input array.

---
+++ digits_in_prime_inndex.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    
+    int size, arr[size];
+    
+    cin >> size;
+    
+    for(int i=0; i<size; i++){
+        cin >> arr[i];
+    }
+    
+    for(int i=0; i<size; i++){
+        int count = 0;
+        
+        for(int j=1; j<=size-1; j++){
+            
+            if(i % j == 0){
+                count++;
+            }
+        }
+        
+        if(count == 2){
+            cout<< arr[i] << " ";
+        }
+    }
+    
+}
